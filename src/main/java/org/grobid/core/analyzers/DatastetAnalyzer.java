a98813d91c1861d74a22925516f92451e5f3d100
Patrice Lopez <patrice.lopez@science-miner.com>
2024-09-14 15:27:24 +0200

    update to grobid 0.8.1

--- src/main/java/org/grobid/core/analyzers/DatastetAnalyzer.java
+++ src/main/java/org/grobid/core/analyzers/DatastetAnalyzer.java
@@ -137,4 +137,9 @@ public class DatastetAnalyzer implements org.grobid.core.analyzers.Analyzer {
     public List<String> retokenizeSubdigits(List<java.lang.String> tokens) {
         return retokenize(tokens);
     }
+
+    public List<LayoutToken> retokenizeFromLayoutToken(List<LayoutToken> tokens) {
+        // already done by this analyzer
+        return tokens;
+    }
 }
